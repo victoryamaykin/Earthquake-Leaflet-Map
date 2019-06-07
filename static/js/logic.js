408cb9ee59dd59e2a1714c7175a5809c8a39f6c8
Victor Yamaykin <victor.yamaykin@gmail.com>
2019-06-07 13:15:42 -0400

    config

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,6 @@
+const API_KEY = "pk.eyJ1IjoidnlhbWF5a2luIiwiYSI6ImNqdzE4amRzbzBqZDg0M29kNDRkejdkNWwifQ.wyQb7NvEKlovRjXRKENS5Q";
+
+
 function createMap(earthquakes) {
 
   // Define streetmap and darkmap layers
